98652c91873515f98c1cba3f6381d12b6fd00382
Chutimasuk01 <chutima18092544@gmail.com>
2023-10-17 12:48:06 +0700

    commit

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ st.header('กรุณาอัพโหลดภาพเอ็กซเรย
 file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
 
 # load classifier
-Model = load_model('./model/pneumonia_classifier.h5')
+model = load_model('./model/pneumonia_classifier.h5')
 
 # load class names
 with open('./model/labels.txt', 'r') as f:
